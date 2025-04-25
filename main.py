523385dd596b909b239e11ebc5f7d30710134f72
Prakhar Singhal <singhalprakhar.rishu@gmail.com>
2025-04-25 23:03:49 +0530

    cleared some comments

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ origins = [
 # Apply CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow your frontend's origin
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
-app.include_router(chat.router) # pending
-app.include_router(suggestions.router) # pending
-app.include_router(conversation.router) # pending
+app.include_router(chat.router) 
+app.include_router(suggestions.router) 
+app.include_router(conversation.router)
 
